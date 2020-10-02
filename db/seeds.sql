807121d350f943213dd4b27c981ea6b556894e50
rancid <robmgraham13@yahoo.com>
2020-10-02 14:15:01 -0700

    file tree added, seesds and myswl data added, connection.js added

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+--  In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (name) VALUES ('Impossible');
+INSERT INTO burgers (name) VALUES ('Western');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+
