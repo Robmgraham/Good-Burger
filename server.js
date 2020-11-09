8c1ecfacbea3809a7535dc4ab6e4f212798e1677
rancid <robmgraham13@yahoo.com>
2020-11-09 11:51:31 -0800

    messing with PORT to get heroku connection

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 var express = require("express");
 
-var PORT =  8000;
+var PORT = process.env.PORT || 8000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -19,6 +16,6 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-app.listen( process.env.PORT || PORT, function() {
+app.listen(PORT, function() {
   console.log("Listening on port:%s", PORT);
 });
